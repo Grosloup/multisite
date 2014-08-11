d463d229a2104517adbcb1e44f91541e4b141a0b
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-08-11 09:22:15 +0200

    sauvegarde

--- src/ZPB/Admin/CommonBundle/Controller/EmailController.php
+++ src/ZPB/Admin/CommonBundle/Controller/EmailController.php
@@ -27,26 +27,89 @@ class EmailController extends BaseController
 {
     public function editAction($id, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('email_edit',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $email = $this->getRepo('ZPBAdminCommonBundle:Email')->find($id);
+        if(!$email){
+            throw $this->createNotFoundException();
+        }
         //TODO
+        return $this->render('ZPBAdminCommonBundle:Email:edit.html.twig');
     }
 
     public function userAddEmailAction($userid, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('user_add_email',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $user = $this->getRepo('ZPBAdminCommonBundle:User')->find($userid);
+        if(!$user){
+            return $this->redirect($this->generateUrl(''));
+            throw $this->createNotFoundException();
+        }
         //TODO
+        return $this->render('ZPBAdminCommonBundle:Email:new.html.twig');
     }
 
     public function userRemoveEmailAction($userid, $emailid, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('user_remove_email',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $email = $this->getRepo('ZPBAdminCommonBundle:Email')->find($emailid);
+        if(!$email){
+            throw $this->createNotFoundException();
+        }
+        $user = $this->getRepo('ZPBAdminCommonBundle:User')->find($userid);
+        if(!$user){
+            throw $this->createNotFoundException();
+        }
         //TODO
+        return $this->redirect($this->generateUrl('zpb_admin_common_security_user_list'));
     }
 
     public function userSetPrimaryEmailAction($userid, $emailid, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('user_setprimary_email',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $email = $this->getRepo('ZPBAdminCommonBundle:Email')->find($emailid);
+        if(!$email){
+            throw $this->createNotFoundException();
+        }
+        $user = $this->getRepo('ZPBAdminCommonBundle:User')->find($userid);
+        if(!$user){
+            throw $this->createNotFoundException();
+        }
         //TODO
+        return $this->redirect($this->generateUrl('zpb_admin_common_security_user_list'));
     }
 
     public function userUnsetPrimaryEmailAction($userid, $emailid, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('user_unsetprimary_email',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $email = $this->getRepo('ZPBAdminCommonBundle:Email')->find($emailid);
+        if(!$email){
+            throw $this->createNotFoundException();
+        }
+        $user = $this->getRepo('ZPBAdminCommonBundle:User')->find($userid);
+        if(!$user){
+            throw $this->createNotFoundException();
+        }
         //TODO
+        return $this->redirect($this->generateUrl('zpb_admin_common_security_user_list'));
     }
 }
--- src/ZPB/Admin/CommonBundle/Controller/PhoneController.php
+++ src/ZPB/Admin/CommonBundle/Controller/PhoneController.php
@@ -27,26 +27,90 @@ class PhoneController extends BaseController
 {
     public function editAction($id, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('phone_edit',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $phone = $this->getRepo('ZPBAdminCommonBundle:PhoneNumber')->find($id);
+        if(!$phone){
+            throw $this->createNotFoundException();
+        }
+
+
         //TODO
+        return $this->render('ZPBAdminCommonBundle:Phone:edit.html.twig');
     }
 
     public function userAddPhoneAction($userid, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('user_add_phone',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $user = $this->getRepo('ZPBAdminCommonBundle:User')->find($userid);
+        if(!$user){
+            throw $this->createNotFoundException();
+        }
         //TODO
+        return $this->render('ZPBAdminCommonBundle:Phone:new.html.twig');
     }
 
     public function userRemovePhoneAction($userid, $phoneid, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('user_remove_phone',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $phone = $this->getRepo('ZPBAdminCommonBundle:PhoneNumber')->find($phoneid);
+        if(!$phone){
+            throw $this->createNotFoundException();
+        }
+        $user = $this->getRepo('ZPBAdminCommonBundle:User')->find($userid);
+        if(!$user){
+            throw $this->createNotFoundException();
+        }
+        return $this->redirect($this->generateUrl('zpb_admin_common_security_user_list'));
         //TODO
     }
 
     public function userSetPrimaryPhoneAction($userid, $phoneid, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('user_setprimary_phone',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $phone = $this->getRepo('ZPBAdminCommonBundle:PhoneNumber')->find($phoneid);
+        if(!$phone){
+            throw $this->createNotFoundException();
+        }
+        $user = $this->getRepo('ZPBAdminCommonBundle:User')->find($userid);
+        if(!$user){
+            throw $this->createNotFoundException();
+        }
+        return $this->redirect($this->generateUrl('zpb_admin_common_security_user_list'));
         //TODO
     }
 
     public function userUnsetPrimaryPhoneAction($userid, $phoneid, Request $request)
     {
+        $csrfProv = $this->getCsrfProvider();
+        $token = $request->query->get('_token', false);
+        if(!$token || !$csrfProv->isCsrfTokenValid('user_unsetprimary_phone',$token)){
+            throw $this->createAccessDeniedException();
+        }
+        $phone = $this->getRepo('ZPBAdminCommonBundle:PhoneNumber')->find($phoneid);
+        if(!$phone){
+            throw $this->createNotFoundException();
+        }
+        $user = $this->getRepo('ZPBAdminCommonBundle:User')->find($userid);
+        if(!$user){
+            throw $this->createNotFoundException();
+        }
+        return $this->redirect($this->generateUrl('zpb_admin_common_security_user_list'));
         //TODO
     }
 }
